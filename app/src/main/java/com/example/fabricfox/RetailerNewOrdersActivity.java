d0a942013e8f69d13a35c2dcd3fcd5c6489c75fa
PC <sami.urk@ucp.edu.pk>
2020-07-01 18:11:27 +0500

    Deadline # 30 successfully completed

--- app/src/main/java/com/example/fabricfox/RetailerNewOrdersActivity.java
+++ app/src/main/java/com/example/fabricfox/RetailerNewOrdersActivity.java
@@ -1,14 +1,102 @@
 package com.example.fabricfox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RetailerNewOrdersActivity extends AppCompatActivity {
+import com.example.fabricfox.Model.RetailerOrders;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RetailerNewOrdersActivity extends AppCompatActivity
+{
+
+    private RecyclerView ordersList;
+    private DatabaseReference ordersRef;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer_new_orders);
+
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+
+        ordersList = findViewById(R.id.orders_list);
+        ordersList.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<RetailerOrders> options =
+                new FirebaseRecyclerOptions.Builder<RetailerOrders>()
+                .setQuery(ordersRef, RetailerOrders.class)
+                .build();
+
+        FirebaseRecyclerAdapter<RetailerOrders, RetailerOrdersViewHolder> adapter =
+                new FirebaseRecyclerAdapter<RetailerOrders, RetailerOrdersViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull RetailerOrdersViewHolder holder, int position, @NonNull RetailerOrders model)
+                    {
+
+                        holder.userName.setText("Name: " + model.getName());
+                        holder.userPhoneNumber.setText("Number: " + model.getPhone());
+                        holder.userTotalPrice.setText("Total Amount: " + model.getTotalAmount());
+                        holder.userDateTime.setText("Order at: " + model.getDate() + "  " + model.getTime());
+                        holder.userShippingAddress.setText("Shipping Address: " + model.getAddress() + "  " + model.getCity());
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public RetailerOrdersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+                    {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.orders_layout , parent, false);
+
+                        return new RetailerOrdersViewHolder(view);
+
+                    }
+                };
+
+        ordersList.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+
+    public static class RetailerOrdersViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView userName, userPhoneNumber, userTotalPrice, userDateTime, userShippingAddress;
+        private Button ShowOrdersBtn;
+
+        public RetailerOrdersViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.order_user_name);
+            userPhoneNumber = itemView.findViewById(R.id.order_phone_number);
+            userTotalPrice = itemView.findViewById(R.id.order_total_price);
+            userDateTime = itemView.findViewById(R.id.order_date_time);
+            userShippingAddress = itemView.findViewById(R.id.order_address_city);
+            ShowOrdersBtn = itemView.findViewById(R.id.show_all_products_btn);
+
+
+
+
+        }
+    }
+
 }
